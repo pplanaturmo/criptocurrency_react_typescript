6054ef4ff142fbf597444f94436f2c5a650e543a
Pedro Plana Turmo <pedro.plana@movicoders.link>
2024-04-17 11:38:40 +0200

    just created

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+
+function App() {
+
+
+  return (
+    <>
+ <div className="container">
+
+<h1 className="app-title"> Cotizador de <span>Criptomonedas</span></h1>
+<div className="content">
+  
+</div>
+ </div>
+    </>
+  )
+}
+
+export default App
